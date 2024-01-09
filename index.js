0148f75019f63db6256fd0c680fdde5ba1b257ff
Themba Mtshelwane <Thembamm3@gmail.com>
2024-01-09 21:07:41 +0200

    subscribe button logic

---
+++ index.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const email = document.getElementById('email').value
+  if (email) {
+    localStorage.setItem('email', email)
+    window.location.href = `./page2.html?email=${encodeURIComponent(email)}`
+  } else {
+    alert('Error')
+  }
+  //   form.reset()
+})
